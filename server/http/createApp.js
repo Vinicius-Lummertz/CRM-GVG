43d58ecad59606a578ea760566576948cea64fe2
Vinicius Lummertz <viniciuslummertzg@gmail.com>
2026-03-20 20:04:17 -0300

    feat(server): enable permissive CORS for frontend access

--- server/http/createApp.js
+++ server/http/createApp.js
@@ -1,11 +1,13 @@
 "use strict";
 
 const express = require("express");
+const cors = require("cors");
 const { registerRoutes } = require("../routes");
 
 function createHttpApp({ publicDir, controllers, middlewares }) {
   const app = express();
 
+  app.use(cors());
   app.use(express.urlencoded({ extended: false }));
   app.use(express.json());
   app.use(express.static(publicDir));
